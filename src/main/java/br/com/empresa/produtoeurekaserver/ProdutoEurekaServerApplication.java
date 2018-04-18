ad25cc9200ee34fb0584a75b90a72729a00758c1
Ramon Amorim <ramon.neogrid@neogrid.com>
2018-04-18 10:58:58 -0300

    Initial Commit

---
+++ src/main/java/br/com/empresa/produtoeurekaserver/ProdutoEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.empresa.produtoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProdutoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoEurekaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/empresa/produtoeurekaserver/ProdutoEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.empresa.produtoeurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdutoEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
